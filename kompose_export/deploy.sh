d858242e4e4fa4146a01585acaa5327e7db56138
Mbauro <redfish32@hotmail.it>
2020-01-30 19:36:40 +0100

    some changes

--- kompose_export/deploy.sh
+++ kompose_export/deploy.sh
@@ -8,6 +8,8 @@ kubectl apply -f spout-pvc.yaml
 kubectl apply -f minio-claim0-persistentvolumeclaim.yaml 
 kubectl apply -f mysql-claim0-persistentvolumeclaim.yaml
 kubectl apply -f prometheus-claim0-persistentvolumeclaim.yaml
+kubectl apply -f consumer-pv.yaml
+kubectl apply -f consumer-pvc.yaml
 echo "Create prometheus config map"
 kubectl apply -f prometheus-config.yaml 
 echo "Exposing services"
@@ -20,6 +22,7 @@ kubectl apply -f zookeeper-service.yaml
 kubectl apply -f spout-service.yaml
 kubectl apply -f application-service.yaml
 kubectl apply -f api-gateway-service.yaml
+kubectl apply -f consumer-service.yaml
 echo "Deploying microservices"
 kubectl apply -f minio-deployment.yaml
 kubectl apply -f mysql-deployment.yaml 
@@ -29,6 +32,7 @@ kubectl apply -f api-gateway-deployment.yaml
 kubectl apply -f application-deployment.yaml 
 kubectl apply -f prometheus-deployment.yaml
 kubectl apply -f spout-deployment.yaml 
+kubectl apply -f consumer-deployment.yaml
 
 
 
--- sparkConsumer/src/main/scala/com/coppolab/App.scala
+++ sparkConsumer/src/main/scala/com/coppolab/App.scala
@@ -2,6 +2,7 @@ package com.coppolab
 
 
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
@@ -9,6 +10,10 @@ import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 import scala.collection.mutable.ListBuffer
+import scala.util.parsing.json
+import scala.util.parsing.json.JSON
+
+
 
 /**
  * Hello world!
@@ -19,12 +24,11 @@ object App {
     // Create context with 30 second batch interval
     val conf = new SparkConf().setAppName("spark-kafka").setMaster("local[2]").set("spark.executor.memory","1g");
     val ssc = new StreamingContext(conf, Seconds(30))
-
     //Env variable from kubernetes deployment yaml
     val URL = sys.env("URL");
     println("URL :"+URL)
 
-    /* Configure Kafka */
+
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> URL,
       "key.deserializer" -> classOf[StringDeserializer],
@@ -36,37 +40,38 @@ object App {
 
     val topics = Array("metrics")
     //Create an input stream that directly pulls messages from Kafka Brokers without using any receiver
+
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent, //Location strategy
       Subscribe[String, String](topics, kafkaParams)
     )
 
-    stream.print();
 
     val lines = stream.map(_.value());
 
-
     lines.print()
 
-
-
+    var counter = 0
 
     val lista = new ListBuffer[String]()
-
     lines.foreachRDD{ rdd => val allFlights = rdd.collect().map(_.toString).mkString(",")
-      println("DENTRO IL CICLO"+allFlights) // prints to the stdout of the driver
+      val parsed = JSON.parseFull(allFlights)
+      println("DENTRO IL CICLO"+parsed) // prints to the stdout of the driver
+      counter = counter + 1
+      println("Counter dentro :"+counter)
 
       if (allFlights!="") {
         lista.append(allFlights)
       }
 
     }
+    println("Counter fuori :"+counter)
 
     // Start the computation
 
-        ssc.start()
-        ssc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
 
 
   }
